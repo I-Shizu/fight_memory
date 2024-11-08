441020ea3be195da5dc45ab3a6470320538e13e0
ikari shizuku <ikari2002pcs@icloud.com>
2024-11-08 16:51:28 +0900

    fvmの導入

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
