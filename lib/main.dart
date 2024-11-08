72bdd917ba1b79661f3b17d402973d6cbdfb3394
ikari shizuku <ikari2002pcs@icloud.com>
2024-11-08 11:46:20 +0900

    firebase_coreを削除

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,14 @@ import 'Pages/top_page.dart';
 import 'Controller/auth_controller.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'firebase_options.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   await FirebaseAppCheck.instance.activate(
   appleProvider: kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
