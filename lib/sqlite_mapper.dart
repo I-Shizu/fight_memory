554f26ba3fca66b11aa8b56a39971882fa75c9fd
ikari shizuku <ikari2002pcs@icloud.com>
2024-11-15 21:52:13 +0900

    splite変換クラスを作成

---
+++ lib/sqlite_mapper.dart
@@ -0,0 +1,19 @@
+import 'Models/post_model.dart';
+
+class SQLiteMapper {
+  static Map<String, dynamic> toSQLite(Post post) {
+    return {
+      'text': post.text,
+      'date': post.date.toIso8601String(),
+      'imageFile': post.imageFile,
+    };
+  }
+
+  static Post fromSQLite(Map<String, dynamic> data) {
+    return Post(
+      text: data['text'] as String,
+      date: DateTime.parse(data['date'] as String),
+      imageFile: data['imageFile'] as String?,
+    );
+  }
+}
