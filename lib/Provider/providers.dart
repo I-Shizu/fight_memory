96bcdc5d909077bc9938a840853863a370a1cda4
ikari shizuku <ikari2002pcs@icloud.com>
2024-12-03 16:01:36 +0900

    通知とテーマの画面部分のみ追加

--- lib/Provider/providers.dart
+++ lib/Provider/providers.dart
@@ -36,3 +36,27 @@ final postImageFileProvider = StateProvider<File?>((ref) {
 
 //写真フォルダへのアクセス許可を管理
 final permissionGrantedProvider = StateProvider<bool>((ref) => false);
+
+//テーマプロバイダの定義
+final themeProvider = StateNotifierProvider<ThemeNotifier, bool>((ref) {
+  return ThemeNotifier();
+});
+
+//通知プロバイダの定義
+final notificationProvider = StateNotifierProvider<NotificationNotifier, bool>((ref) {
+  return NotificationNotifier();
+});
+
+// テーマ状態を管理する StateNotifier
+class ThemeNotifier extends StateNotifier<bool> {
+  ThemeNotifier() : super(false); // 初期値はライトテーマ(false)
+
+  void toggleTheme() => state = !state;
+}
+
+// 通知状態を管理する StateNotifier
+class NotificationNotifier extends StateNotifier<bool> {
+  NotificationNotifier() : super(true); // 初期値は通知オン(true)
+
+  void toggleNotification() => state = !state;
+}
