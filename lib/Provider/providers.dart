35a6095839e2b49160d7db55b251f1eec075aa3c
ikari shizuku <ikari2002pcs@icloud.com>
2024-12-06 00:33:54 +0900

    微修正

--- lib/Provider/providers.dart
+++ lib/Provider/providers.dart
@@ -44,19 +44,17 @@ final themeProvider = StateNotifierProvider<ThemeNotifier, bool>((ref) {
   return ThemeNotifier();
 });
 
-//通知プロバイダの定義
-final notificationProvider = StateNotifierProvider<NotificationNotifier, bool>((ref) {
-  return NotificationNotifier();
-});
-
-
-
 // テーマ状態を管理するStateNotifier
 class ThemeNotifier extends StateNotifier<bool> {
   ThemeNotifier() : super(false); // 初期値はライトテーマ(false)
 
   void toggleTheme() => state = !state;
 }
+
+//通知プロバイダの定義
+final notificationProvider = StateNotifierProvider<NotificationNotifier, bool>((ref) {
+  return NotificationNotifier();
+});
 
 // 通知状態を管理するStateNotifier
 class NotificationNotifier extends StateNotifier<bool> {
